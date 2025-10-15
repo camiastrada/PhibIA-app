456c787f607e55873b68d7c39fc64bc7b0407e83
TrinidadSA <aguirretrini97@gmail.com>
2025-10-15 00:26:22 -0300

    integrate frontend, backend, and database with Docker Compose

--- phibIA-app/frontend/src/App.tsx
+++ phibIA-app/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./layouts/DahboardLayout";
 import Login from "./pages/Login";
