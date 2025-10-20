6391c795ed916d46c9b21b5b0be381eb65b462f7
Agostina Rodriguez <agosrodriguezdev@gmail.com>
2025-10-20 17:21:40 -0300

    fronted home view fixed and upload link added

--- phibIA-app/frontend/src/layouts/DahboardLayout.tsx
+++ phibIA-app/frontend/src/layouts/DahboardLayout.tsx
@@ -3,15 +3,22 @@ import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
 import Bottombar from "../components/Bottombar";
 import { Outlet } from "react-router-dom";
-import Background from '/images/bgMainImage.jpg'
+import Background from "/images/bgMainImage.jpg";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex h-screen w-screen items-center"style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'top' }} >
+    <div
+      className="flex h-screen w-screen items-center"
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
       <Topbar />
-      <Bottombar /> 
+      <Bottombar />
       <Sidebar />
-      <div className="flex justify-center items-center w-full h-full overflow-y-clip overflow-x-hidden" >
+      <div className="flex justify-center items-center w-full h-full overflow-y-clip overflow-x-hidden ">
         <Outlet />
       </div>
     </div>
--- phibIA-app/frontend/src/pages/Home.tsx
+++ phibIA-app/frontend/src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import "../styles/App.css";
 import logo from "../assets/singleLogo.png";
-import specie1 from"../assets/speciesShadow/RhinellaArenarumShadow1.png";
-import specie2 from"../assets/speciesShadow/OdontophrynusAsperShadow2.png";
-import specie3 from"../assets/speciesShadow/BoanaPulchellaShadow3.png";
-import specie4 from"../assets/speciesShadow/CeratophrysCranwelliShadow4.png";
-import specie5 from"../assets/speciesShadow/LeptodactylusGracilisShadow5.png";
-
+import specie1 from "../assets/speciesShadow/RhinellaArenarumShadow1.png";
+import specie2 from "../assets/speciesShadow/OdontophrynusAsperShadow2.png";
+import specie3 from "../assets/speciesShadow/BoanaPulchellaShadow3.png";
+import specie4 from "../assets/speciesShadow/CeratophrysCranwelliShadow4.png";
+import specie5 from "../assets/speciesShadow/LeptodactylusGracilisShadow5.png";
 
 function Home() {
   const [listening, setListenig] = useState(false);
-  const [isImport, setIsImport] = useState(false);
+  const [_isImport, setIsImport] = useState(false);
   const [actualShadow, setActualShadow] = useState(specie1);
   const shadowsList = [specie1, specie2, specie3, specie4, specie5];
 
@@ -37,62 +38,129 @@ function Home() {
   }, [listening]);
 
   return (
-    <div className="flex flex-col justify-around items-center w-9/10 h-4/5 md:h-7/10 md:w-4/5 bg-white/85 border-2 border-white backdrop-blur-xs rounded-3xl">
+    <div className="flex-col justify-around items-center w-9/10 md:w-4/5 bg-white/85 border-2 border-white backdrop-blur-xs rounded-3xl p-4">
       <div id="titleSection">
-        <h1 className="text-5xl font-bold mb-6 text-[#43A047]">¡Comienza a grabar!</h1> {/* 02372E*/}
+        <h1 className="text-5xl font-bold mb-6 text-[#43A047]">
+          ¡Comienza a grabar!
+        </h1>{" "}
+        {/* 02372E*/}
         <p className="text-xl mb-4 text-center px-10 font-normal font-sans">
-          Acercate al anfibio  y graba su canto para detectar su especie
+          Acercate al anfibio y graba su canto para detectar su especie
         </p>
       </div>
-      <div id="panelSection " className="flex flex-col justify-around gap-6 items-center"> 
-          <div id="panel" className="bg-[#004D40] rounded-full size-60 flex justify-center items-center">
-            {listening ? (
-              <img src={actualShadow} alt="Species Shadow" className="w-50 h-auto" />
-            ) : (
-              <img src={logo} alt="Logo" className="w-60 h-auto" />
-            )}
-          </div>
+      <div
+        id="panelSection "
+        className="flex flex-col justify-around gap-6 items-center"
+      >
+        <div
+          id="panel"
+          className="bg-[#004D40] rounded-full size-60 flex justify-center items-center"
+        >
           {listening ? (
-            <p> Escuchando... </p>
-          ):
-          (
-          <p>
-            Comienza a grabar para obtener resultados
-          </p>
+            <img
+              src={actualShadow}
+              alt="Species Shadow"
+              className="w-50 h-auto"
+            />
+          ) : (
+            <img src={logo} alt="Logo" className="w-60 h-auto" />
           )}
+        </div>
+        {listening ? (
+          <p> Escuchando... </p>
+        ) : (
+          <p>Comienza a grabar para obtener resultados</p>
+        )}
       </div>
 
-      <div id="buttonsSection" className="flex flex-col justify-around items-center">
-        <button type="button" onClick={() => {setListenig(!listening); setIsImport(false)}} className={`flex flex-row justify-center cursor-pointer ${listening? "bg-transparent text-red-600 animate-pulse" : "bg-[#43A047] rounded-xl shadow-lg hover:shadow-xl hover:bg-[#357a38] text-white w-60 px-6 py-3 text-lg font-semibold"}`}>
+      <div
+        id="buttonsSection"
+        className="flex flex-col justify-around items-center"
+      >
+        <button
+          type="button"
+          onClick={() => {
+            setListenig(!listening);
+            setIsImport(false);
+          }}
+          className={`flex flex-row justify-center cursor-pointer ${
+            listening
+              ? "bg-transparent text-red-600 animate-pulse"
+              : "bg-[#43A047] rounded-xl shadow-lg hover:shadow-xl hover:bg-[#357a38] text-white w-60 px-6 py-3 text-lg font-semibold"
+          }`}
+        >
           {listening ? (
             <>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-15">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-15"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z"
+                />
               </svg>
             </>
-          ):
-          (
+          ) : (
             <>
-              <p>
-                Grabar
-              </p>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+              <p>Grabar</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+                />
               </svg>
             </>
           )}
         </button>
-        <button type="button" onClick={() => {setIsImport(true); setListenig(false)}} className={`mt-4 text-[#004D40] hover:text-[#02372E] flex-row justify-center items-center ${listening ? "hidden" : "flex"}`}>
-          Importar grabación
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 inline-block ml-1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-          </svg>
-        </button>
-        
-      </div>
 
-      
+        <Link to="/upload">
+          <button
+            type="button"
+            onClick={() => {
+              setIsImport(true);
+              setListenig(false);
+            }}
+            className={`mt-4 text-[#004D40] hover:text-[#02372E] flex-row justify-center items-center ${
+              listening ? "hidden" : "flex"
+            }`}
+          >
+            Importar grabación
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-4 inline-block ml-1"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
