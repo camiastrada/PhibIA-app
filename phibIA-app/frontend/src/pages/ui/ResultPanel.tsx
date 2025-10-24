3d905196b4a4ef8e6ab3d55d58490669a768736d
LucasLillo03 <lucaslillo0207@gmail.com>
2025-10-24 20:05:37 -0300

    The actual shape of the species is now displayed when obtaining a result

--- phibIA-app/frontend/src/pages/ui/ResultPanel.tsx
+++ phibIA-app/frontend/src/pages/ui/ResultPanel.tsx
@@ -1,10 +1,15 @@
 import {useEffect, useState} from "react";
 import logo from "../../assets/singleLogo.png";
-import specie1 from "../../assets/speciesShadow/RhinellaArenarumShadow1.png";
-import specie2 from "../../assets/speciesShadow/OdontophrynusAsperShadow2.png";
-import specie3 from "../../assets/speciesShadow/BoanaPulchellaShadow3.png";
-import specie4 from "../../assets/speciesShadow/CeratophrysCranwelliShadow4.png";
-import specie5 from "../../assets/speciesShadow/LeptodactylusGracilisShadow5.png";
+import specieShadow1 from "../../assets/speciesShadow/RhinellaArenarumShadow1.png";
+import specie1 from "../../assets/species/RhinellaArenarum1.png";
+import specieShadow2 from "../../assets/speciesShadow/OdontophrynusAsperShadow2.png";
+import specie2 from "../../assets/species/OdontophrynusAsper2.png";
+import specieShadow3 from "../../assets/speciesShadow/BoanaPulchellaShadow3.png";
+import specie3 from "../../assets/species/BoanaPulchella3.png";
+import specieShadow4 from "../../assets/speciesShadow/CeratophrysCranwelliShadow4.png";
+import specie4 from "../../assets/species/CeratophrysCranwelli4.png";
+import specieShadow5 from "../../assets/speciesShadow/LeptodactylusGracilisShadow5.png";
+import specie5 from "../../assets/species/LeptodactylusGracilis5.png";
 
 interface Params{
   listening: boolean;
@@ -12,10 +17,13 @@ interface Params{
   specie?: string; 
 }
 export default function ResultPanel({listening, prediction , specie} : Params){
-    const [actualShadow, setActualShadow] = useState(specie1);
-    const shadowsList = [specie1, specie2, specie3, specie4, specie5];
-
-    console.log(specie)
+    const [actualShadow, setActualShadow] = useState(specieShadow1);
+    const [actualSpecie, setActualSpecie] = useState(specie1);
+    const [showSpecie, setShowSpecie] = useState(false);
+    const [thinking, setThinking] = useState(false)
+    const shadowsList = [specieShadow1, specieShadow2, specieShadow3, specieShadow4, specieShadow5];
+    const speciesList = [specie1, specie2, specie3, specie4, specie5];
+    
 
     const changeShadow = () => {
         setActualShadow((prev) => {
@@ -26,20 +34,31 @@ export default function ResultPanel({listening, prediction , specie} : Params){
     };
 
     useEffect(() => {
-      if (!listening) return;
+      if (!listening || prediction ) return;
+      
+      const id = setInterval(changeShadow, 500);
+      setShowSpecie(false)
+      setThinking(true)
+      return () => clearInterval(id);
+    }, [listening]);
 
+    useEffect(() => {
       if (prediction && specie) {
         const index = parseInt(specie) - 1;
         if (index >= 0 && index < shadowsList.length) {
           setActualShadow(shadowsList[index]);
+          setActualSpecie(speciesList[index]);
+          setThinking(false)
+          setShowSpecie(false);
+
+          const timeout = setTimeout(() => {
+            setShowSpecie(true);
+          }, 2000);
+
+          return () => clearTimeout(timeout);
         }
-        return; // 👈 Esto corta el efecto antes de crear el intervalo
       }
-
-      const id = setInterval(changeShadow, 500);
-      return () => clearInterval(id);
-    }, [listening, prediction, specie]);
-
+    }, [prediction, specie]);
 
 
     return(
@@ -51,21 +70,29 @@ export default function ResultPanel({listening, prediction , specie} : Params){
             id="panel"
             className="bg-[#004D40] rounded-full size-40 md:size-60 flex justify-center items-center"
           >
-            {listening || prediction ? (
-              <img
-                src={actualShadow}
-                alt="Species Shadow"
-                className="w-30 md:w-50 h-auto"
-              />
-            ) : (
-              <img src={logo} alt="Logo" className="w-60 h-auto" />
-            )}
+            {listening || prediction || thinking ? (
+                <div className="flex justify-center items-center">
+                  <img
+                    src={actualShadow}
+                    alt="Species Shadow"
+                    className={`w-30 md:w-50 h-auto absolute z-10 transition-opacity duration-1000 ${
+                      showSpecie && !listening ? "opacity-0" : "opacity-100"
+                    }`}
+                    />
+                  <img
+                    src={showSpecie? actualSpecie : actualShadow}
+                    alt="Specie"
+                    className={`w-30 h-auto md:w-50 absolute transition-opacity duration-1000 ${
+                      showSpecie && prediction? "opacity-100" : "opacity-0"
+                    }`}
+                    />
+                </div> 
+                
+                ) : (
+                  <img src={logo} alt="Logo" className="w-60 h-auto" />
+                )
+            }
           </div>
-          {listening ? (
-            <p className="text-md"> Escuchando... </p>
-          ) : (
-            <p className="text-md">Comienza a grabar para obtener resultados</p>
-          )}
         </div>
     )
 }
