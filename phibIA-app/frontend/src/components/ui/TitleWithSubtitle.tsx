3318b2b9ea1c75b1fc31adf109363943e6d18072
LucasLillo03 <lucaslillo0207@gmail.com>
2025-10-25 23:10:55 -0300

    now TitleWithSubtitle receive a optional color for the title

--- phibIA-app/frontend/src/components/ui/TitleWithSubtitle.tsx
+++ phibIA-app/frontend/src/components/ui/TitleWithSubtitle.tsx
@@ -1,14 +1,16 @@
 interface Text{
     title?: string; 
+    titleColor?: string; 
     subtitle?: string; 
 }
-export default function TitleWithSubtitle({title, subtitle} : Text){
+export default function TitleWithSubtitle({title, subtitle, titleColor} : Text){
     return(
         <>
-        <h1 className="text-2xl md:text-5xl font-bold mb-6 text-[#43A047]">
+        <h1 className="text-2xl md:text-5xl font-bold text-[#43A047]"
+            style={{color: titleColor}}>
             {title}
         </h1>
-        <p className="text-md md:text-xl mb-4 text-center px-10 font-normal font-sans">
+        <p className="text-md md:text-xl my-4 text-center px-10 font-normal font-sans">
             {subtitle}
         </p>
         </>
