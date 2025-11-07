5ae61f1cf7085bf17dac8f748759124c4c25e2ee
Agostina Rodriguez <agosrodriguezdev@gmail.com>
2025-11-07 02:24:00 -0300

    add route to display map

--- phibIA-app/frontend/src/App.tsx
+++ phibIA-app/frontend/src/App.tsx
@@ -4,19 +4,19 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import FrogsMap from "./pages/Maps";
 import "./styles/App.css";
 function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<DashboardLayout />}>
           <Route path="" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
-
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/frogs-map" element={<FrogsMap />} />
       </Routes>
     </Router>
   );
